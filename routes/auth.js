98574e5975c5321285cd37d213f4134ce302243d
dinh0910 <dinhpoor0910@gmail.com>
2022-11-20 19:11:15 +0700

    Vinh Committed

--- routes/auth.js
+++ routes/auth.js
@@ -53,6 +53,40 @@ router.post('/dangnhap', function(req, res){
 	}
 })
 
+// GET: Đăng ký
+router.get('/dangky', function (req, res) {
+    res.render('dangky', { title: 'Đăng ký tài khoản' })
+})
+
+//POST: Đăng ký
+router.post('/dangky', function(req, res){
+	var errors = validationResult(req)
+	if(!errors.isEmpty()) {
+		if(req.file) fs.unlink(req.file.path, function(err){});
+		res.render('dangky', {
+			title: 'Đăng ký tài khoản',
+			errors: errors.array()
+		})
+	} else {
+		var fileName = ''
+		if(req.file) fileName = req.file.filename
+		var data = {
+			TenTaiKhoan: req.body.TenTaiKhoan,
+			MatKhau: bcrypt.hashSync(req.body.XacNhanMatKhau, saltRounds)
+		}
+		var sql = 'INSERT INTO taikhoan SET ?'
+		conn.query(sql, data, function(error, results){
+			if(error) {
+				req.session.error = error
+				res.redirect('/error')
+			} else {
+				req.session.success = 'Đã đăng ký tài khoản thành công.'
+				res.redirect('/success')
+			}
+		})
+	}
+})
+
 // GET: Đăng xuất
 router.get('/dangxuat', function(req, res){
 	if(req.session.MaTaiKhoan){
