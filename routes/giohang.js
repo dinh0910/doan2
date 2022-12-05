e8a84d3af287f8a3d4edd56bcaa3c0be788f65b1
dinh0910 <dinhpoor0910@gmail.com>
2022-12-05 17:43:00 +0700

    Vinh Committed

--- routes/giohang.js
+++ routes/giohang.js
@@ -6,8 +6,7 @@ router.get('/', function(req, res){
     var sql = 'SELECT * FROM danhmuc ORDER BY MaDanhMuc;\
                 SELECT d.DanhMuc, l.* FROM danhmuc d, loaisanpham l\
                 WHERE d.MaDanhMuc = l.MaDanhMuc\
-                ORDER BY MaDanhMuc;\
-                SELECT * FROM sanpham'
+                ORDER BY MaDanhMuc;'
     conn.query(sql, function(error, results){
         if(error){
             res.send('/error')
@@ -19,6 +18,42 @@ router.get('/', function(req, res){
             })
         }
     }) 
+})
+
+//POST: Ao
+var cart_data, cart = {}
+router.post('/', function(req, res){
+    cart = req.session.cart
+    if(!cart){
+        cart = req.session.cart = {}
+    }
+    var id = req.body.MaSanPham
+    var count = parseInt(req.body.count, 10)
+
+    cart[id] = (cart[id] || 0) + count
+
+    var ids = Object.keys(cart)
+    if(ids.length > 0){
+        conn.query('SELECT * FROM danhmuc ORDER BY MaDanhMuc;\
+                    SELECT d.DanhMuc, l.* FROM danhmuc d, loaisanpham l\
+                    WHERE d.MaDanhMuc = l.MaDanhMuc\
+                    ORDER BY MaDanhMuc;\
+                    SELECT * FROM sanpham WHERE MaSanPham IN (' + ids + ')', function(err, results){
+            if(err){
+                res.send('/error')
+            } else {
+                res.render('giohang', {
+                    title: 'Giỏ hàng',
+                    danhmuc: results[0],
+                    loaisanpham: results[1],
+                    cart_data: results[2],
+                    cart: cart
+                })
+            }
+        })
+    } else {
+        res.redirect('/ao')
+    }
 })
 
 module.exports = router
